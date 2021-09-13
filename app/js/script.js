71bc91b8a5f2dde9a8a3522791bc6d449061534e
Dominika <fud.dominika@gmail.com>
2021-09-13 18:29:35 +0200

    Checking email pattern

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,17 @@
-console.log("Hel")
+document.querySelector(".email__submit").addEventListener("click", (e) => {
+    const emailInput = document.querySelector(".email__input").value;
+    const re = new RegExp(/^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/);
+    if (!re.exec(emailInput)) {
+        e.preventDefault();
+        if (document.querySelector(".email__error--disabled") && document.querySelector(".email__msg--disabled")) {
+            document.querySelector(".email__error--disabled").classList = "email__error email__error--animated";
+            document.querySelector(".email__msg--disabled").classList = "email__msg";
+            const inputElem = document.querySelector(".email__input");
+            inputElem.style.borderColor = "var(--soft-red)";
+            inputElem.style.borderWidth = "2px";
+        } else {
+            document.querySelector(".email__error").classList.remove("email__error--animated");
+            setTimeout(() => document.querySelector(".email__error").classList.add("email__error--animated"), 10);
+        }
+    }
+});
